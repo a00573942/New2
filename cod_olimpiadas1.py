73a72585382f810d8dac62fe5ad8bbf2ea1ea895
a00573942 <145026211+a00573942@users.noreply.github.com>
2023-09-21 08:03:27 -0600

    Create cod_olimpiadas1.py

---
+++ cod_olimpiadas1.py
@@ -0,0 +1,29 @@
+def suma(a,b):
+  x = a+b
+  return x
+
+def resta(a,b):
+  x= a-b
+  return x
+
+print("dame el primer número:")
+a=int(input())
+print("dame el segundo número:")
+b= int(input())
+print("la suma da", suma(a,b), "y la resta da", resta(a,b))
+
+
+def multiplicación(a,b):
+  x= a*b
+  return x
+
+def división(a,b)
+  x= a/b
+  return x
+
+print("dame el primer número")
+a=int(input())
+print("dame el segundo número")
+b=int(input())
+
+print("el resultado de la multiplicación es", multiplicación(a,b),"el resultado de la división es", división(a,b))
